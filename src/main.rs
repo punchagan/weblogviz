3f8f78c51e5e6ba59e4f2ccc44ab15809ff8dba7
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-10-31 21:23:41 +0530

    Start using clap for command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+#[macro_use]
+extern crate clap;
 extern crate weblogviz;
 
-use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    let yaml = load_yaml!("cli.yml");
+    let m = clap::App::from_yaml(yaml).get_matches();
+    if let Some(path) = m.value_of("INPUT") {
+        if let Err(e) = weblogviz::run(&String::from(path)) {
+            println!("Application error: {}", e);
+            process::exit(1);
+        }
+    } else {
         println!("Missing argument: path to log file");
-        process::exit(1);
-    }
-
-    if let Err(e) = weblogviz::run(&args[1]) {
-        println!("Application error: {}", e);
         process::exit(1);
     }
 }
